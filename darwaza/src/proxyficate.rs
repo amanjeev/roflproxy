38ce6445043e61ec9b2fa81dd6ba0e84de193cce
Amanjeev Sethi <aj@amanjeev.com>
2020-03-22 16:04:06 -0400

    WIP: compiles => saves
    Signed-off-by: Amanjeev Sethi <aj@amanjeev.com>

--- darwaza/src/proxyficate.rs
+++ darwaza/src/proxyficate.rs
@@ -1,24 +1,30 @@
 use std::collections::HashMap;
 use std::error::Error;
+
 use surf::{Request as RequestAsClient, Response as ResponseAsClient};
 use tide::{Request as RequestAsServer, Response as ResponseAsServer};
 use url::Url;
+
+use crate::urlmap::UrlMap;
 
 pub struct Proxificate;
 
 impl Proxificate {
     /// Receives request from client, returns corresponding request object
     /// to the downstream server
-    pub async fn map_request(
+    pub fn map_request(
         mut request_from_client: RequestAsServer<()>,
     ) -> RequestAsClient<http_client::isahc::IsahcClient> {
         let method = request_from_client.method().clone();
 
         //TODO: Get the target server URL
-        let target_server_url = Url::parse("http://127.0.0.1:8000/404.html").unwrap();
+        let target_server_url = Url::get_target_server_url(request_from_client)
+            .unwrap_or(Url::parse("http://127.0.0.1:8000/404.html").unwrap());
 
         let request_for_downstream = RequestAsClient::new(method, target_server_url);
 
         request_for_downstream
     }
+
+    pub fn convert_to_response() -> Result<ResponseAsClient, Box<dyn Error>> {}
 }
--- darwaza/src/urlmap.rs
+++ darwaza/src/urlmap.rs
@@ -1,3 +1,58 @@
+use std::collections::HashMap;
+use std::error::Error;
+
+use tide::{Request as RequestAsServer, Response as ResponseAsServer};
+use url::Url;
+
 /// Trait to extend functionality of Url crate
 /// with Url mapping using a mapping file
-pub trait UrlMap {}
+pub trait UrlMap {
+    /// Returns HashMap of the endpoints to target server mapping
+    fn map_uri_to_target() -> HashMap<&'static str, &'static str> {
+        // TODO: get this map from config
+        let mut uri_to_target_map: HashMap<&str, &str> = HashMap::new();
+        uri_to_target_map.insert("/home", "http://127.0.0.1:8000");
+        uri_to_target_map.insert("/about", "http://127.0.0.1:8000");
+
+        uri_to_target_map
+    }
+
+    /// Returns a target server URL given the `request` object
+    /// If the mapping is
+    ///     ```
+    ///     "/home"  => "foo.com:1234"
+    ///     "/staff" => "bar.org:4567"
+    ///     ```
+    /// and the request is for
+    ///     `/staff/jdoe`
+    /// then, the target server is `foo.com:1234/jdoe`
+    fn get_target_server_url(request: RequestAsServer<()>) -> Result<Url, Box<dyn Error>> {
+        let incoming_request_uri = request.uri().clone();
+        // get the target from uri segment using the map
+        let uri_map = Self::map_uri_to_target();
+        let mut result_server = "".to_string();
+        let mut result_url = "".to_string();
+        dbg!(incoming_request_uri.clone());
+        for (uri, server) in uri_map.iter() {
+            if incoming_request_uri
+                .to_string()
+                .as_str()
+                .starts_with(uri.to_string().as_str())
+            {
+                dbg!("Matched: ", uri.clone());
+                result_server = server.to_string();
+                result_url = incoming_request_uri.to_string().replace(uri, "");
+                dbg!(result_server.clone(), result_url.clone());
+            } else {
+                dbg!("No match for: ", uri.clone());
+            }
+        }
+        let result_target = Url::parse(result_server.as_str())?.join(result_url.as_str())?;
+
+        dbg!("The final Target>> ", result_target.clone());
+
+        Ok(result_target)
+    }
+}
+
+impl UrlMap for Url {}
