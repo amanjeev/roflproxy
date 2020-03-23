e753bdee95b02cd79c42197668585544abdfaccd
Amanjeev Sethi <aj@amanjeev.com>
2020-03-22 22:05:41 -0400

    Config: setting some config types

--- darwaza/src/config.rs
+++ darwaza/src/config.rs
@@ -1,1 +1,22 @@
-// Configuration management for rofl
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+pub struct RoflServerConfig {
+    pub addr: SocketAddr, // ip:port
+}
+
+impl Default for RoflServerConfig {
+    fn default() -> Self {
+        RoflServerConfig {
+            addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12666),
+        }
+    }
+}
+
+impl Clone for RoflServerConfig {
+    fn clone(&self) -> Self {
+        RoflServerConfig { addr: self.addr }
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct RoflRouteConfig {}
